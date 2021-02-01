54ea1670e769fad939e04725a120d090beea502f
Sakshem Jain <61968230+Sakshem@users.noreply.github.com>
2021-02-01 22:43:11 +0530

    Banking system in java

---
+++ Java/BankingSystem/Bank.java
@@ -0,0 +1,75 @@
+package com.sakshem;
+
+public class Bank {
+    //data members or fields
+    private int accountNumber;
+    private double balance;
+    private String customerName;
+    private String email;
+    private long phoneNumber;
+
+    //constructor
+    public Bank() {
+        this(56789, 0.0, "Default name", "default email", 00000000L);
+        System.out.println("empty constructor called!");
+    }
+    public Bank(int accountNumber, double balance, String customerName, String email, long phoneNumber) {
+        System.out.println("Account constructor with parameters called!");
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+        this.customerName = customerName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        //alternate way of assigning the values to the fields
+        /*setAccountNumber(accountNumber);
+        setBalance(balance);
+        setCustomerName(customerName);
+        setEmail(email);
+        setPhoneNumber(phoneNumber);*/
+    }
+    public Bank(String customerName, String email, long phoneNumber) {
+        this(99999, 0.0, customerName, email, phoneNumber);
+        //called the main parameterized constructor by passing default account number and balance and customerName and others
+    }
+
+    //methods or functions
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+    public double getBalance() {
+        return balance;
+    }
+    public String getCustomerName() {
+        return customerName;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public long getPhoneNumber() {
+        return phoneNumber;
+    }
+    /*
+    * NOTE: this keyword when attached to a variable means we are calling a field of a class and we represent it as this.field (example)
+    * but in c++ when we want to use this keyword to call (in a way) the data members of the class we access them as this->field (example)
+    * and in Java we use this(parameters) for other different parameters constructor but in c++ we cannot do this.
+    * we can assign the default value in the parameters section like this-> Rectangle(int length = 1, int breadth = 1)
+    * In this if length or breadth is not passed they will take that as a default value (in this case 1)
+    */
+    public void setAccountNumber(int accountNumber) { this.accountNumber = accountNumber; }
+    public void setBalance(double balance) { this.balance = balance; }
+    public void setCustomerName(String customerName) { this.customerName = customerName; }
+    public void setEmail(String email) {this.email = email;}
+    public void setPhoneNumber(long phoneNumber) { this.phoneNumber = phoneNumber; }
+    public void depositFunds(double amountAdded) {
+        balance += amountAdded;
+        System.out.println("Deposit of " + amountAdded + " successful. Total balance is " + this.balance);
+    }
+    public void withdrawFunds(double amountTaken) {
+        if((balance - amountTaken) >= 0) {
+            balance -= amountTaken;
+            System.out.println("Withdrawal amount " + amountTaken + " processed. Remaining balance " + this.balance);
+        }
+        else
+            System.out.println("Only " + balance + " available, withdrawal not processed.");
+    }
+}
---
+++ Java/BankingSystem/Main.java
@@ -0,0 +1,36 @@
+package com.sakshem;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    //a new class bank
+        Bank sakshemsAccount = new Bank();
+        sakshemsAccount.setAccountNumber(12345);
+        sakshemsAccount.setBalance(0.00);
+        sakshemsAccount.setCustomerName("Sakshem Jain");
+        sakshemsAccount.setEmail("sakshemjain7960@gmail.com");
+        sakshemsAccount.setPhoneNumber(7000047369L);
+
+        //withdrawing and depositing funds
+        sakshemsAccount.withdrawFunds(100.0);
+
+        sakshemsAccount.depositFunds(50.0);
+        sakshemsAccount.withdrawFunds(100.0);
+
+        sakshemsAccount.depositFunds(51.0);
+        sakshemsAccount.withdrawFunds(100.0);
+        System.out.println();
+        //creating a new account (or object of class Bank with calling the constructor and initializing the value there itself)
+        Bank bobsAccount = new Bank(2030, 0.00, "Bob Brown", "myemail@bob.com", 9826239185L);
+        System.out.println("Account number: " + bobsAccount.getAccountNumber());
+        System.out.println("Current Balance: " + bobsAccount.getBalance());
+        System.out.println("Customer Name: " + bobsAccount.getCustomerName());
+        System.out.println("Customer's Email address: " + bobsAccount.getEmail());
+        System.out.println("Customer Phone Number: " + bobsAccount.getPhoneNumber());
+
+        //creating a new account in the bank for Tim
+        System.out.println();
+        Bank timsAccount = new Bank("Tim", "tim232@gmail.com", 324038332423L);
+        System.out.println(timsAccount.getAccountNumber() + " " + timsAccount.getCustomerName() + " " + timsAccount.getEmail() + " " + timsAccount.getPhoneNumber());
+    }
+}
