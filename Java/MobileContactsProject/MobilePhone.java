1a07c88a7bc56e567b5fadd0aec93f3b76a221cd
Sakshem Jain <61968230+Sakshem@users.noreply.github.com>
2021-03-05 13:42:00 +0530

    Update MobilePhone.java

--- Java/MobileContactsProject/MobilePhone.java
+++ Java/MobileContactsProject/MobilePhone.java
@@ -20,6 +20,11 @@ public class MobilePhone {
             System.out.println(oldContact.getName () + " ,was not found.");
             return false;
         }
+        else if (findContact (newContact.getName ()) != -1) {
+            System.out.println ("Contact with name " + newContact.getName () + " already exists.");
+            System.out.println ("Update was not successful.");
+            return false;
+        }
         this.myContacts.set (foundPosition, newContact);
         System.out.println(oldContact.getName () + " ,was replaced with " + newContact.getName());
         return true;
